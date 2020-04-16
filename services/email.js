44c5338649ee6d7facf998172fc2598dac9c8533
Sebastian Hernandez <sebhernandezram@gmail.com>
2020-04-16 00:20:59 -0500

    Add sendgrid

---
+++ services/email.js
@@ -0,0 +1,34 @@
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const compose = async (emailData, type) => {
+  if (type === 'contact') {
+    emailData.templateId = 'd-186e35707107415488f73c9e734777fd';
+    const result = await send(emailData);
+    return result;
+  } else if (type === 'order') {
+    emailData.templateId = 'd-e271caa3216f4c7688a721209843c328';
+    const result = await send(emailData);
+    return result;
+  } else {
+    throw new Error('Wrong type');
+  }
+};
+
+const send = async (emailData) => {
+  const { to, from, subject, body, templateId } = emailData;
+  const msg = {
+    to: to,
+    from: from,
+    templateId: templateId,
+    dynamic_template_data: {
+      subject: subject,
+      body: body,
+    },
+  };
+  const res = await sgMail.send(msg);
+  const { statusCode } = res[0];
+  return statusCode;
+};
+
+module.exports = { compose };
